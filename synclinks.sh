84a127863952eaa50b80344f37b90341bef285cd
Andrea Leopardi <an.leopardi@gmail.com>
2013-07-08 17:52:44 +0200

    Relocate some git stuff

--- synclinks.sh
+++ synclinks.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-for link in $HOME/.dotfiles/.{bash_profile,bashrc,vim,vimrc,gitconfig}; do
+# classic dotfiles
+for link in $HOME/.dotfiles/.{bash_profile,bashrc,vim,vimrc}; do
 	ln -sv $link $HOME
 done
+
+# stuff hidden inside folders
+ln -sv $HOME/.dotfiles/git/.gitconfig $HOME
