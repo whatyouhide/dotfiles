ea35e03133c71395cf4fd4b6cacaa98aa4a6fc71
Andrea Leopardi <an.leopardi@gmail.com>
2013-11-23 19:16:11 +0100

    Add TextMate properties file

--- synclinks.sh
+++ synclinks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # classic dotfiles
-for link in $HOME/.dotfiles/.{bash_profile,bashrc,inputrc,vim,vimrc,remediarc}; do
+for link in $HOME/.dotfiles/.{bash_profile,bashrc,inputrc,vim,vimrc,remediarc,tm_properties}; do
 	ln -sv $link $HOME
 done
 
