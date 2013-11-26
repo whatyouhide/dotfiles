828e54326ae8a8c607b21a8f96593dd7d46c2e41
Andrea Leopardi <an.leopardi@gmail.com>
2013-11-26 16:45:06 +0100

    The big 'refactoring'

--- synclinks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# classic dotfiles
-for link in $HOME/.dotfiles/.{bash_profile,bashrc,inputrc,vim,vimrc,remediarc,tm_properties}; do
-	ln -sv $link $HOME
-done
-
-# stuff hidden inside folders
-ln -sv $HOME/.dotfiles/git/.gitconfig $HOME
