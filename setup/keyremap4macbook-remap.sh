d6be05be4f170287177f179ddaced9f8897d4a26
Andrea Leopardi <an.leopardi@gmail.com>
2014-04-27 23:40:45 +0200

    Put keyremap4macbook stuff back in its place

---
+++ setup/keyremap4macbook-remap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+
+$cli set remap.uk_saneuklayout 1
+/bin/echo -n .
+$cli set repeat.initial_wait 250
+/bin/echo -n .
+$cli set repeat.wait 25
+/bin/echo -n .
+/bin/echo
