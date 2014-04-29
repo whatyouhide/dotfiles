54fb9c52e71c5d8d9789604313f7adf45671c69c
Andrea Leopardi <an.leopardi@gmail.com>
2014-04-29 18:14:58 +0200

    CAPSLOCK to Ctrl when pressed with other keys...
    ...but to Esc when pressed alone! This should be really fun :).
    It's an experimental hack (provided by keyremap4macbook), I still have to see if
    I find myself comfortable with it (mainly if it annoys me in any situation where
    I want to use Ctrl on its own, and I don't recall many of those situations).
    Keep in mind that I use 'jj' to exit insert mode in vim, and I'm pretty sure
    exiting Insert mode is where I use Esc the most.

--- setup/keyremap4macbook-remap.sh
+++ setup/keyremap4macbook-remap.sh
@@ -2,10 +2,14 @@
 
 cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
 
-$cli set remap.uk_saneuklayout 1
+$cli set repeat.wait 25
 /bin/echo -n .
 $cli set repeat.initial_wait 250
 /bin/echo -n .
-$cli set repeat.wait 25
+$cli set remap.uk_saneuklayout 1
+/bin/echo -n .
+$cli set repeat.consumer_wait 15
+/bin/echo -n .
+$cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
 /bin/echo
