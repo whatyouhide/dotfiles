446a21afaf1d2791e2547a22106639acb4ac0dc1
Andrea Leopardi <an.leopardi@gmail.com>
2014-06-26 13:37:57 +0200

    Update keyremap4macbook options

--- keyremap4macbook/keyremap4macbook-remap.sh
+++ keyremap4macbook/keyremap4macbook-remap.sh
@@ -2,16 +2,16 @@
 
 cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
 
+$cli set remap.layout2us 1
+/bin/echo -n .
 $cli set repeat.wait 25
 /bin/echo -n .
 $cli set repeat.initial_wait 250
-/bin/echo -n .
-$cli set remap.uk_saneuklayout 1
 /bin/echo -n .
 $cli set repeat.consumer_wait 15
 /bin/echo -n .
 $cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
-$cli set remap.escape2eject 1
+$cli set private.swap_cmd_option_windows 1
 /bin/echo -n .
 /bin/echo
