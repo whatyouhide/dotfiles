678619258ea39ea6aa66ea332230e908e035bd4b
Andrea Leopardi <an.leopardi@gmail.com>
2014-12-06 12:39:07 +0100

    Clean up some shell code

--- vim/install.sh
+++ vim/install.sh
@@ -5,13 +5,15 @@ ROOT="$DOTFILES/vim"
 # Link the main vim directory (after removing the symlink in ~).
 rm -f "$HOME/.vim" && ln -sv "$ROOT" "$HOME/.vim"
 
+# Temporary plugins.
+touch "$ROOT/tmp-plugins.vim"
+
 # vim-plug
 _autoload="$ROOT/autoload"
 mkdir -p "$_autoload"
 
 if [[ ! -s "$_autoload/plug.vim" ]]; then
   echo "Installing vim-plug..."
-
   _url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-  curl --fail --progress-bar --output "$_autoload/plug.vim" "$_url"
+  curl --fail --silent "$_url" > "$_autoload/plug.vim"
 fi
