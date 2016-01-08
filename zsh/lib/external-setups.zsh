a64878f7fe6e33197c0f6749b985da0c3007e9c7
Andrea Leopardi <whatyouhide@Andreas-MacBook-Pro.local>
2016-01-08 15:49:30 +0100

    Clean up some zsh setups

--- zsh/lib/external-setups.zsh
+++ zsh/lib/external-setups.zsh
@@ -3,11 +3,3 @@
 
 # Direnv
 command-exists direnv && eval "$(direnv hook zsh)"
-
-# rbenv
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-# exenv
-export PATH="$HOME/.exenv/bin:$PATH"
-eval "$(exenv init -)"
