aebbbbdbbb8c9c7708011e39fb4cf4f42ec9f6ed
Andrea Leopardi <an.leopardi@gmail.com>
2016-01-23 18:04:15 +0100

    Don't write over ~/.gitconfig

--- git/install.sh
+++ git/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "Writing ~/.gitconfig (which will include ~/.gitconfig_shared)"
-
-cat <<EOF > "$HOME/.gitconfig"
+if [[ ! -s "$HOME/.gitconfig" ]]; then
+    echo "Writing ~/.gitconfig (which will include ~/.gitconfig_shared)"
+    cat <<EOF > "$HOME/.gitconfig"
 [include]
-	path = ~/.gitconfig_shared
+  path = ~/.gitconfig_shared
 EOF
+fi
