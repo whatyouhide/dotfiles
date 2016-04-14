2377eb78380a65f6b4ad7c51decb9310a6209936
Andrea Leopardi <an.leopardi@gmail.com>
2016-04-14 22:13:03 +0200

    Explicitly separate 'git' and 'hub'

--- zsh/lib/functions.zsh
+++ zsh/lib/functions.zsh
@@ -1,11 +1,3 @@
-# Use git in a cooler way: 'g' for status, g {args} for normal git.
-# If 'hub' is available, use 'hub' as git.
-git() {
-  local cmd='git'
-  command-exists hub && cmd='hub'
-  ([[ $# > 0 ]] && command "$cmd" "$@") || command "$cmd" status
-}
-
 # Quickly open a new tmux session.
 sess() {
   local default_name=$(date +"%T")
