8fd71c892ca376423af85b38827b04b5b9fe3588
Andrea Leopardi <an.leopardi@gmail.com>
2016-11-20 20:18:57 +0100

    Fix the Homebrew installation script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-if [ ! type "$1" >/dev/null 2>&1 ]; do
+if [ ! type "$1" >/dev/null 2>&1 ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install git
+    brew install hub
+    brew install coreutils
+    brew install findutils
+    brew install readline
+    brew install homebrew/dupes/grep
+    brew install direnv
+    brew install reattach-to-user-namespace
+    brew install the_silver_searcher
+    brew install ssh-copy-id
+
+    brew doctor
 fi
-
-brew update -v
-brew upgrade
-
-brew install git
-brew install hub
-brew install coreutils
-brew install findutils
-brew install readline
-brew install homebrew/dupes/grep
-brew install direnv
-brew install reattach-to-user-namespace
-brew install the_silver_searcher
-brew install ssh-copy-id
-
-brew doctor
