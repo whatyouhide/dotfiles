90ae786db2d0d092ae16680115777e016fffe91c
Andrea Leopardi <an.leopardi@gmail.com>
2019-04-26 09:26:37 +0200

    Improve Git installation workflow

--- git/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ ! -s "$HOME/.gitconfig" ]]; then
-    echo "Writing ~/.gitconfig (which will include ~/.gitconfig_shared)"
-    cat <<EOF > "$HOME/.gitconfig"
-[include]
-  path = ~/.gitconfig_shared
-EOF
-fi
