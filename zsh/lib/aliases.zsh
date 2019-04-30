da926df94da9706f2867915cfbc2764f3bd8fe95
Andrea Leopardi <an.leopardi@gmail.com>
2019-04-30 11:56:10 +0200

    Clean up zsh config

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -10,7 +10,6 @@ alias l="ls -lAFh ${colorflag}"
 
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias localip="ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'"
 
 # Recursively delete `.DS_Store` files
 alias clean-ds-stores="find . -name '*.DS_Store' -type f -ls -delete"
