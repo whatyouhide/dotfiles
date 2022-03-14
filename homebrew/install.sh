a9548d9c033eeefc25a0e496f97e0b80eb0d1a09
Andrea Leopardi <an.leopardi@gmail.com>
2022-03-14 15:22:18 +0100

    Fix brew bundle

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if ! type "$1" >/dev/null 2>&1; then
+if ! type "brew" >/dev/null 2>&1; then
     echo "Homebrew not found, install it first"
     exit 1
 fi
 
-brew bundle install
+brew bundle install --file homebrew/Brewfile
 
