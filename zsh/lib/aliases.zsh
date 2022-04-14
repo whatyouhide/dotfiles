305368802158a07380449ca3eeb8d0c97aeee351
Andrea leopardi <an.leopardi@gmail.com>
2022-04-14 11:31:19 +0200

    Use "exa" as an "ls" replacement

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -1,17 +1,11 @@
-# `ls`.
-# Detect which `ls` flavor is in use.
-# List all files colorized in long format, including dot files
-if ls --color > /dev/null 2>&1; then
-  colorflag="--color" # GNU `ls`
-else
-  colorflag="-G"      # OS X `ls`
-fi
-alias l="ls -lAFh ${colorflag}"
+# `ls` replacement.
+alias l="exa --long --binary --group --classify --git"
+
+# `cat` replacement.
+alias cat="bat --paging=never --theme=base16 --style=header-filename,header-filesize,grid"
 
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 # Recursively delete `.DS_Store` files
 alias clean-ds-stores="find . -name '*.DS_Store' -type f -ls -delete"
-
-alias cat="bat --paging=never --theme=zenburn"
