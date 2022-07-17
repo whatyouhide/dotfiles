de846fdffdd1ef192f092c3808b079e67c44e0dd
Andrea leopardi <an.leopardi@gmail.com>
2022-07-17 10:46:18 +0200

    Update the "l" alias

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -1,5 +1,5 @@
 # `ls` replacement.
-alias l="exa --long --binary --group --classify --git"
+alias l="exa --long --binary --group --classify --git --all"
 
 # `cat` replacement.
 alias cat="bat --paging=never --theme=base16 --style=header-filename,header-filesize,grid"
