aa3218cfe38b99348f6059fcb4eaff250f0d9ee6
Andrea Leopardi <an.leopardi@gmail.com>
2024-04-19 08:28:02 +0200

    Simplify zsh prompt

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -4,8 +4,5 @@ alias l="eza --long --binary --group --classify --git --all"
 # `cat` replacement.
 alias cat="bat --paging=never --theme=base16 --style=header-filename,header-filesize,grid"
 
-# IP addresses
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-
 # Recursively delete `.DS_Store` files
 alias clean-ds-stores="find . -name '*.DS_Store' -type f -ls -delete"
