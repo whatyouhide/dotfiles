ea3d285f705fbc7283d28a9a5d01b0958ca7d42c
Andrea Leopardi <an.leopardi@gmail.com>
2024-07-16 19:16:29 +0200

    Better Homebrew install script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,4 +5,21 @@ if ! type "brew" >/dev/null 2>&1; then
     exit 1
 fi
 
-brew bundle install --file homebrew/Brewfile --no-lock --cleanup
+install() {
+    brew bundle install --file homebrew/Brewfile --no-lock --cleanup
+}
+
+echo ""
+read -r -p "Do you want to install Homebrew packages? (Y/n) " confirmation
+
+case $confirmation in
+"")
+    install
+    ;;
+"Y")
+    install
+    ;;
+"n")
+    echo "Skipping Homebrew packages then!"
+    ;;
+esac
